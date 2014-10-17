4620cf1af7af0e2b8a8cb4a27273cc800f7ff33c
John Stiles <hotdog92892@yahoo.com>
2014-10-16 22:37:11 -0400

    Board randomly generated.

--- Game1.cs
+++ Game1.cs
@@ -1,5 +1,6 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using RogueSharp;
 
 namespace SimpleGame
 {
@@ -12,8 +13,9 @@ namespace SimpleGame
         SpriteBatch spriteBatch;
         Vector2 wallVector = new Vector2(0, 0);
         Vector2 floorVector = new Vector2(32, 32);
-        Texture2D wallTexture;
-        Texture2D floorTexture;
+        Texture2D _wall;
+        Texture2D _floor;
+        private IMap _map;
 
         public Game1()
         {
@@ -30,6 +32,8 @@ namespace SimpleGame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            IMapCreationStrategy<Map> mapCreationStrategy = new RandomRoomsMapCreationStrategy<Map>(50, 30, 100, 7, 3);
+            _map = Map.Create(mapCreationStrategy);
 
             base.Initialize();
         }
@@ -44,8 +48,8 @@ namespace SimpleGame
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            wallTexture = this.Content.Load<Texture2D>("Wall.png");
-            floorTexture = this.Content.Load<Texture2D>("Floor.png");
+            _wall = this.Content.Load<Texture2D>("Wall.png");
+            _floor = this.Content.Load<Texture2D>("Floor.png");
         }
 
         /// <summary>
@@ -78,9 +82,23 @@ namespace SimpleGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            spriteBatch.Begin();
-            spriteBatch.Draw(wallTexture, wallVector);
-            spriteBatch.Draw(floorTexture, floorVector);
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
+
+            const int sizeOfSprites = 32;
+            //const float scale = .5f;
+            foreach (Cell cell in _map.GetAllCells())
+            {
+                var position = new Vector2(cell.X * sizeOfSprites * 1f, cell.Y * sizeOfSprites * 1f);
+                if (cell.IsWalkable)
+                {
+                    spriteBatch.Draw(_floor, position, null, null, null, 0.0f, new Vector2(1f, 1f), Color.White);
+                }
+                else
+                {
+                    spriteBatch.Draw(_wall, position, null, null, null, 0.0f, new Vector2(1f, 1f), Color.White);
+                }
+            }
+
             spriteBatch.End();
 
             base.Draw(gameTime);
