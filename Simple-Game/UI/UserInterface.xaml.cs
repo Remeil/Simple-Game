f077737571f7521f48ba1eca8e47dfd7dc3cbc34
Remeil <stilesjohnathan21@gmail.com>
2015-07-24 19:33:14 -0400

    First attempt at UI

---
+++ Simple-Game/UI/UserInterface.xaml.cs
@@ -0,0 +1,52 @@
+// -----------------------------------------------------------
+//  
+//  This file was generated, please do not modify.
+//  
+// -----------------------------------------------------------
+namespace EmptyKeys.UserInterface.Generated {
+    using System;
+    using System.CodeDom.Compiler;
+    using System.Collections.ObjectModel;
+    using EmptyKeys.UserInterface;
+    using EmptyKeys.UserInterface.Data;
+    using EmptyKeys.UserInterface.Controls;
+    using EmptyKeys.UserInterface.Controls.Primitives;
+    using EmptyKeys.UserInterface.Input;
+    using EmptyKeys.UserInterface.Media;
+    using EmptyKeys.UserInterface.Media.Animation;
+    using EmptyKeys.UserInterface.Media.Imaging;
+    using EmptyKeys.UserInterface.Shapes;
+    using EmptyKeys.UserInterface.Renderers;
+    using EmptyKeys.UserInterface.Themes;
+    
+    
+    [GeneratedCodeAttribute("Empty Keys UI Generator", "1.7.5.0")]
+    public partial class UserInterface : UIRoot {
+        
+        private Grid e_0;
+        
+        private TextBlock e_1;
+        
+        public UserInterface(int width, int height) : 
+                base(width, height) {
+            Style style = RootStyle.CreateRootStyle();
+            style.TargetType = this.GetType();
+            this.Style = style;
+            this.InitializeComponent();
+        }
+        
+        private void InitializeComponent() {
+            // e_0 element
+            this.e_0 = new Grid();
+            this.Content = this.e_0;
+            this.e_0.Name = "e_0";
+            // e_1 element
+            this.e_1 = new TextBlock();
+            this.e_0.Children.Add(this.e_1);
+            this.e_1.Name = "e_1";
+            this.e_1.Text = "Hello World";
+            Grid.SetRow(this.e_1, 0);
+            FontManager.Instance.AddFont("Segoe UI", 12F, FontStyle.Regular, "Segoe_UI_9_Regular");
+        }
+    }
+}
